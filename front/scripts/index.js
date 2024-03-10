95d0ee8af40f1e7c4dfe9e37952b1a9680a03a89
tomysykess <tomysykess@gmail.com>
2024-03-10 16:45:03 -0300

    module.exports

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -1,44 +1,7 @@
-class Peliculas {
-    constructor(title, year, director, duration, genre, rate, poster) {
-        this.title = title
-        this.year = year
-        this.director = director
-        this.duration = duration
-        this.genre = genre
-        this.rate = rate
-        this.poster = poster
-    }
-}
-
-function toHtml(objeto) {
-    const { title, year, director, duration, genre, rate, poster } = objeto
-
-    const nuevoDiv = document.createElement("div");
-    nuevoDiv.classList.add("nuevoDiv")
-
-    nuevoDiv.innerHTML =
-        `<div >
-    <h3 class="classH3">${title}</h3>
-        <h4 class="classH4">${year}</h4>
-        <img class="classPoster" src="${poster}" alt="">
-        <div class="izq">
-           <div>
-              <h4 class="classH4"><b>Director: </b>${director}</h4>
-              <h4 class="classH4"><b>Duración: </b>${duration}</h4>
-           </div>   
-        </div>   
-        <div class="der">
-           <div>
-           <h4 class="classH4">★${rate}</h4>
-           <h4 class="classH4">${genre}</h4>
-           </div>   
-           </div> 
-           </div>  
-           `
-
-    return nuevoDiv
-}
 /* CLASE 2HW */
+
+const Peliculas = require('./peliculasModule.js');
+const { toHtml } = require('./DOMModule.js');
 function movieToHtml(data) {
     const contenedorPeliculas = document.getElementById("divFinal");
     contenedorPeliculas.innerHTML = '';
